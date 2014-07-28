bd843132c2f4208bf06186e38cce6495f1351cd0
Tim Koopmans <tim.koops@gmail.com>
2014-07-28 16:49:30 +1000

    bunch of new tests

---
+++ benchmarks/spec/throughput/rate.rb
@@ -0,0 +1,19 @@
+require 'ruby-jmeter'
+
+test do
+
+  threads count: 500,
+          rampup: 60,
+          scheduler: true,
+          duration: 540,
+          continue_forever: true,
+          delayedStart: true  do
+
+    constant_timer delay: 60000
+
+    dummy_sampler name: '1829kB', url: 'https://s3-ap-southeast-2.amazonaws.com/flood-loadtest/1829kb.html'
+  end
+end.flood(ENV['FLOOD_API_TOKEN'], {
+  name: 'Throughput Test Single Node',
+  tag_list: 'benchmarks',
+  privacy_flag: 'public'})
